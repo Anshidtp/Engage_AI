581d7bb140766d133ced116bbe7bb525428d4eba
Anshidtp <shaanshid07@gmail.com>
2025-09-22 00:27:59 +0530

    still getting invocation error, trying test from base

--- api/index.py
+++ api/index.py
@@ -1,3 +1,15 @@
-from app.main import app
+# from app.main import app
+# from mangum import Mangum
+# handler = Mangum(app, lifespan="off")
+
+from fastapi import FastAPI
 from mangum import Mangum
-handler = Mangum(app, lifespan="off")
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello from FastAPI on Vercel!"}
+
+# Export the handler Vercel looks for
+handler = Mangum(app)
