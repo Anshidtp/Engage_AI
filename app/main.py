46e247fe3e858f97f0963123e6c605fe3c6170f7
Anshidtp <shaanshid07@gmail.com>
2025-09-22 01:54:06 +0530

    deployement started on render

--- app/main.py
+++ app/main.py
@@ -56,8 +56,7 @@ async def root():
     }
  
 
-# handler = app 
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run("app.main:app", host ="0.0.0.0", port=8000, reload=True)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host ="0.0.0.0", port=8000, reload=True)
