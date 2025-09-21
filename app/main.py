96441e863a0bb4c779eb50102070b6c258d93e98
Anshidtp <shaanshid07@gmail.com>
2025-09-21 23:37:18 +0530

    resolving internal server error

--- app/main.py
+++ app/main.py
@@ -55,8 +55,8 @@ async def root():
     }
  
 
-# handler = app 
+handler = app 
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run("app.main:app", host ="0.0.0.0", port=8000, reload=True)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host ="0.0.0.0", port=8000, reload=True)
